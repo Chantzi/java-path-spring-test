7280173192fa266db8b1cd9a7115198436e6ef0a
Angeliki Chantzithanou <aggexan@gmail.com>
2021-05-19 09:40:49 +0300

    Initial commit

---
+++ src/main/java/gr/codelearn/java/path/spring/test/JavaPathSpringTestApplication.java
@@ -0,0 +1,13 @@
+package gr.codelearn.java.path.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPathSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPathSpringTestApplication.class, args);
+    }
+
+}
